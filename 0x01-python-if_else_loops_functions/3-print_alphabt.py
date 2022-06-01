50c2fde205f72d11d1de567870b1fddeccb17f2e
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-06-01 07:04:26 -0700

    Program to print ASCII alphabets except e and q in lowercase without using any module

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for leet in range(97, 123):
-    if leet == ord(e) or leet == otd(q):
+    if leet == ord(e) or leet == ord(q):
         continue
     print("{:c}".format(leet), end='')

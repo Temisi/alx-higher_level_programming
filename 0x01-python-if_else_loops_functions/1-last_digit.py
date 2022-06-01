526009180515dc04c4c79de52ab630b4f33c7eb9
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-06-01 04:38:20 -0700

    Program to check if the last digit of a number is greater than 5 or less

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastNum = number % 10
+if number < 10:
+    lastNum = number % -10
+print(f"Last digit of {number:d} is {lastNum:d} and is ", end= "")
+if lastNum > 5:
+    print("greater than 5")
+elif lastNum == 0:
+    print("0")
+elif lastNum < 6 and != 0:
+    print("less than 6 and not 0")

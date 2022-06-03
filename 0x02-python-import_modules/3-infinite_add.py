940f647bfd0445ad3807aa9ea2f5b73f54ccb7cd
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-06-03 11:14:38 -0700

    program that prints the result of the addition of all argument in a code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__main__":
+
+
+def main():
     mas = 0
     for b in range(1, len(argv)):
         mas += int(argv[b])
     print("{:d}".format(mas))
+
+
+if __name__ == "__main__":
+    main()
